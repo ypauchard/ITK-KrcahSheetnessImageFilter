b0b700498a1224769c053e3110dffed7a720ac35
Thomas Fitze <fitz@zhaw.ch>
2014-10-02 16:09:08 +0200

    started femur optimized implementation

---
+++ src/DescoteauxSheetnessImageFilterFemur.h
@@ -0,0 +1,219 @@
+/*=========================================================================
+Program: Insight Segmentation & Registration Toolkit
+Module: itkDescoteauxSheetnessImageFilter.h
+Language: C++
+Date: $Date$
+Version: $Revision$
+Copyright (c) Insight Software Consortium. All rights reserved.
+See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the above copyright notices for more information.
+=========================================================================*/
+#ifndef __itkDescoteauxSheetnessImageFilterFemur_h
+#define __itkDescoteauxSheetnessImageFilterFemur_h
+
+#include "itkUnaryFunctorImageFilter.h"
+#include "vnl/vnl_math.h"
+
+namespace itk {
+/** \class DescoteauxSheetnessImageFilterFemur
+*
+* \brief Computes a measure of Sheetness from the Hessian Eigenvalues optimzed for usage in femur / pelvis segmentation.
+*
+* Based on the "Sheetness" measure proposed by Decouteaux et. al.
+*
+* M.Descoteaux, M.Audette, K.Chinzei, el al.:
+* "Bone enhancement filtering: Application to sinus bone segmentation
+* and simulation of pituitary surgery."
+* In: MICCAI. (2005) 9–16
+*
+* and the work by Krcah et. al.
+*
+* M. Krcah, G. Szekely, R. Blanc:
+* "Fully automatic and fast segmentation of the femur bone from 3d-CT images with no shape prior"
+*/
+    namespace Function {
+        template<class TInput, class TOutput>
+        class FemurSheetness {
+        public:
+            FemurSheetness() {
+                // suggested values by Krcah el. al.
+                m_Alpha = 0.5;
+                m_Beta = 0.5;
+                m_Gamma = 0.25;
+                m_DetectBrightSheets = true;
+            }
+
+            ~FemurSheetness() {
+            }
+
+            bool operator!=(const FemurSheetness &) const {
+                return false;
+            }
+
+            bool operator==(const FemurSheetness &other) const {
+                return !(*this != other);
+            }
+
+            inline TOutput operator()(const TInput &A) {
+                double sheetness = 0.0;
+                double a1 = static_cast<double>( A[0] );
+                double a2 = static_cast<double>( A[1] );
+                double a3 = static_cast<double>( A[2] );
+                double l1 = vnl_math_abs(a1);
+                double l2 = vnl_math_abs(a2);
+                double l3 = vnl_math_abs(a3);
+//
+// Sort the values by their absolute value.
+// At the end of the sorting we should have
+//
+// l1 <= l2 <= l3
+//
+                if (l2 > l3) {
+                    double tmpl = l3;
+                    l3 = l2;
+                    l2 = tmpl;
+                    double tmpa = a3;
+                    a3 = a2;
+                    a2 = tmpa;
+                }
+                if (l1 > l2) {
+                    double tmp = l1;
+                    l1 = l2;
+                    l2 = tmp;
+                    double tmpa = a1;
+                    a1 = a2;
+                    a2 = tmpa;
+                }
+                if (l2 > l3) {
+                    double tmp = l3;
+                    l3 = l2;
+                    l2 = tmp;
+                    double tmpa = a3;
+                    a3 = a2;
+                    a2 = tmpa;
+                }
+                if (this->m_DetectBrightSheets) {
+                    if (a3 > 0.0) {
+                        return static_cast<TOutput>( sheetness );
+                    }
+                }
+                else {
+                    if (a3 < 0.0) {
+                        return static_cast<TOutput>( sheetness );
+                    }
+                }
+//
+// Avoid divisions by zero (or close to zero)
+//
+                if (static_cast<double>( l3 ) < vnl_math::eps || static_cast<double>( l2 ) < vnl_math::eps) {
+                    return static_cast<TOutput>( sheetness );
+                }
+
+                const double T = l1 + l2 + l3; // http://en.wikipedia.org/wiki/Trace_%28linear_algebra%29#Eigenvalue_relationships
+                const double Rsheet = l2 / l3;
+                const double Rnoise = (l1 + l2 + l3) / T;
+                const double Rtube = l1 / (l2 * l3);
+
+                sheetness = (-vnl_math_sgn(a3));
+                sheetness *= vcl_exp(-(Rsheet * Rsheet) / (m_Alpha * m_Alpha));
+                sheetness *= vcl_exp(-(Rtube * Rtube) / (m_Beta * m_Beta));
+                sheetness *= (1.0 - vcl_exp(-(Rnoise * Rnoise) / (m_Gamma * m_Gamma)));
+
+                return static_cast<TOutput>( sheetness );
+            }
+
+            void SetAlpha(double value) {
+                this->m_Alpha = value;
+            }
+
+            void SetBeta(double value) {
+                this->m_Beta = value;
+            }
+
+            void SetGamma(double value) {
+                this->m_Gamma = value;
+            }
+
+            void SetDetectBrightSheets(bool value) {
+                this->m_DetectBrightSheets = value;
+            }
+
+            void SetDetectDarkSheets(bool value) {
+                this->m_DetectBrightSheets = !value;
+            }
+
+        private:
+            double m_Alpha;
+            double m_Beta;
+            double m_Gamma;
+            bool m_DetectBrightSheets;
+        };
+    }
+    template<class TInputImage, class TOutputImage>
+    class ITK_EXPORT DescoteauxSheetnessImageFilterFemur :
+            public UnaryFunctorImageFilter<TInputImage, TOutputImage,
+                    Function::FemurSheetness<typename TInputImage::PixelType,
+                            typename TOutputImage::PixelType> > {
+    public:
+/** Standard class typedefs. */
+        typedef DescoteauxSheetnessImageFilterFemur Self;
+        typedef UnaryFunctorImageFilter<
+                TInputImage, TOutputImage,
+                Function::FemurSheetness<
+                        typename TInputImage::PixelType,
+                        typename TOutputImage::PixelType> > Superclass;
+        typedef SmartPointer<Self> Pointer;
+        typedef SmartPointer<const Self> ConstPointer;
+/** Method for creation through the object factory. */
+        itkNewMacro(Self);
+/** Runtime information support. */
+        itkTypeMacro(DescoteauxSheetnessImageFilterFemur,
+                UnaryFunctorImageFilter);
+
+/** Set the normalization term for sheetness */
+        void SetSheetnessNormalization(double value) {
+            this->GetFunctor().SetAlpha(value);
+        }
+
+/** Set the normalization term for bloobiness. */
+        void SetBloobinessNormalization(double value) {
+            this->GetFunctor().SetBeta(value);
+        }
+
+/** Set the normalization term for noise. */
+        void SetNoiseNormalization(double value) {
+            this->GetFunctor().SetGamma(value);
+        }
+
+        void SetDetectBrightSheets(bool value) {
+            this->GetFunctor().SetDetectBrightSheets(value);
+        }
+
+        void SetDetectDarkSheets(bool value) {
+            this->GetFunctor().SetDetectDarkSheets(value);
+        }
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+/** Begin concept checking */
+        typedef typename TInputImage::PixelType InputPixelType;
+        itkConceptMacro(BracketOperatorsCheck,
+                (Concept::BracketOperator<InputPixelType, unsigned int, double>));
+        itkConceptMacro(DoubleConvertibleToOutputCheck,
+                (Concept::Convertible<double, typename TOutputImage::PixelType>));
+/** End concept checking */
+#endif
+    protected:
+        DescoteauxSheetnessImageFilterFemur() {
+        }
+
+        virtual ~DescoteauxSheetnessImageFilterFemur() {
+        }
+
+    private:
+        DescoteauxSheetnessImageFilterFemur(const Self &); //purposely not implemented
+        void operator=(const Self &); //purposely not implemented
+    };
+} // end namespace itk
+#endif
--- src/main.cxx
+++ src/main.cxx
@@ -10,6 +10,7 @@
 #include "itkMaximumImageFilter.h"
 
 #include "DescoteauxSheetnessImageFilter.h"
+#include "DescoteauxSheetnessImageFilterFemur.h"
 
 // vtk
 #include "QuickView.h"
@@ -27,12 +28,13 @@ typedef itk::ImageFileReader<InputImageType> FileReaderType;
 // filters
 typedef itk::CurvatureAnisotropicDiffusionImageFilter<InputImageType, InternalImageType> DiffusionFilterType;
 typedef itk::HessianRecursiveGaussianImageFilter<InternalImageType> HessianFilterType;
-typedef typename HessianFilterType::OutputImageType HessianImageType;
-typedef typename HessianImageType::PixelType HessianPixelType;
+typedef HessianFilterType::OutputImageType HessianImageType;
+typedef HessianImageType::PixelType HessianPixelType;
 typedef itk::FixedArray<double, HessianPixelType::Dimension> EigenValueArrayType;
 typedef itk::Image<EigenValueArrayType, IMAGE_DIMENSION> EigenValueImageType;
 typedef itk::SymmetricEigenAnalysisImageFilter<HessianImageType, EigenValueImageType> EigenAnalysisFilterType;
 typedef itk::DescoteauxSheetnessImageFilter<EigenValueImageType, OutputImageType> SheetnessFilterType;
+typedef itk::DescoteauxSheetnessImageFilterFemur<EigenValueImageType, OutputImageType> FemurSheetnessFilterType;
 typedef itk::RescaleIntensityImageFilter<OutputImageType, OutputImageType> RescaleFilterType;
 typedef itk::Functor::Abs<InternalImageType::PixelType, InternalImageType::PixelType> AbsFunctorType;
 typedef itk::UnaryFunctorImageFilter<InternalImageType, InternalImageType, AbsFunctorType> AbsFilterType;
@@ -43,6 +45,8 @@ typedef itk::BinaryFunctorImageFilter<OutputImageType, OutputImageType, OutputIm
 void process(char *);
 FileReaderType::Pointer readImage(char *path);
 OutputImageType::Pointer calculateSheetness(InputImageType::Pointer input, float sigma);
+
+OutputImageType::Pointer calculateFemurSheetness(InputImageType::Pointer input, float sigma);
 
 // data
 QuickView viewer;
@@ -60,21 +64,30 @@ void process(char *imagePath) {
     FileReaderType::Pointer inputReader = readImage(imagePath);
     InputImageType::Pointer input = inputReader->GetOutput();
 
-    // get the sheetness for both sigma
-    InternalImageType::Pointer resultSigma075 = calculateSheetness(input, 0.75);
-    InternalImageType::Pointer resultSigma100 = calculateSheetness(input, 1.0);
+    // get the sheetness for both sigma with default sheetness implementation
+    InternalImageType::Pointer resultSigma075Default = calculateSheetness(input, 0.75);
+    InternalImageType::Pointer resultSigma100Default = calculateSheetness(input, 1.0);
 
-    // result(x,y) = max(resultSigma075(x,y), resultSigma100(x,y))
-    MaximumFilterType::Pointer m_MaximumFilter = MaximumFilterType::New();
-    m_MaximumFilter->SetInput1(resultSigma075);
-    m_MaximumFilter->SetInput2(resultSigma100);
-    m_MaximumFilter->Update();
+    // result(x,y) = max(resultSigma075Default(x,y), resultSigma100Default(x,y))
+    MaximumFilterType::Pointer m_MaximumFilterDefault = MaximumFilterType::New();
+    m_MaximumFilterDefault->SetInput1(resultSigma075Default);
+    m_MaximumFilterDefault->SetInput2(resultSigma100Default);
+    m_MaximumFilterDefault->Update();
+
+    // get the sheetness for both sigma with femur optimized sheetness implementation
+    InternalImageType::Pointer resultSigma075Femur = calculateFemurSheetness(input, 0.75);
+    InternalImageType::Pointer resultSigma100Femur = calculateFemurSheetness(input, 1.0);
+
+    // result(x,y) = max(resultSigma075Femur(x,y), resultSigma100Femur(x,y))
+    MaximumFilterType::Pointer m_MaximumFilterFemur = MaximumFilterType::New();
+    m_MaximumFilterFemur->SetInput1(resultSigma075Femur);
+    m_MaximumFilterFemur->SetInput2(resultSigma100Femur);
+    m_MaximumFilterFemur->Update();
 
     // output
     viewer.AddImage(input.GetPointer(), true, "input");
-    viewer.AddImage(resultSigma075.GetPointer(), true, "abs(r1), sigma=0.75");
-    viewer.AddImage(resultSigma100.GetPointer(), true, "abs(r2), sigma=1.0");
-    viewer.AddImage(m_MaximumFilter->GetOutput(), true, "r(x,y) = max(r1(x,y)), r2(x,y)");
+    viewer.AddImage(m_MaximumFilterDefault->GetOutput(), true, "default sheetness");
+    viewer.AddImage(m_MaximumFilterFemur->GetOutput(), true, "femur optimzed sheetness");
     viewer.Visualize();
 }
 
@@ -98,7 +111,49 @@ OutputImageType::Pointer calculateSheetness(InputImageType::Pointer input, float
     m_SheetnessFilter->SetSheetnessNormalization(0.5); // = alpha
     m_SheetnessFilter->SetBloobinessNormalization(0.5); // = beta
     m_SheetnessFilter->SetNoiseNormalization(0.25); // = gamma
-    //m_SheetnessFilter->SetDetectBrightSheets(true);
+    m_SheetnessFilter->SetDetectBrightSheets(true);
+
+    // abs
+    AbsFilterType::Pointer m_AbsFilter = AbsFilterType::New();
+
+    // rescale
+    RescaleFilterType::Pointer m_RescaleFilter = RescaleFilterType::New();
+    m_RescaleFilter->SetOutputMinimum(0.0);
+    m_RescaleFilter->SetOutputMaximum(1.0);
+
+    // connect pipeline
+    m_DiffusionFilter->SetInput(input);
+    m_HessianFilter->SetInput(m_DiffusionFilter->GetOutput());
+    m_EigenAnalysisFilter->SetInput(m_HessianFilter->GetOutput());
+    m_SheetnessFilter->SetInput(m_EigenAnalysisFilter->GetOutput());
+    m_AbsFilter->SetInput(m_SheetnessFilter->GetOutput());
+    m_RescaleFilter->SetInput(m_AbsFilter->GetOutput());
+
+    m_RescaleFilter->Update();
+    return m_RescaleFilter->GetOutput();
+}
+
+OutputImageType::Pointer calculateFemurSheetness(InputImageType::Pointer input, float sigma) {
+    // anisotropic diffusion
+    DiffusionFilterType::Pointer m_DiffusionFilter = DiffusionFilterType::New();
+    m_DiffusionFilter->SetNumberOfIterations(10);
+    m_DiffusionFilter->SetTimeStep(0.120);
+    m_DiffusionFilter->SetConductanceParameter(2.0);
+
+    // hessian
+    HessianFilterType::Pointer m_HessianFilter = HessianFilterType::New();
+    m_HessianFilter->SetSigma(sigma);
+
+    // eigen analysis
+    EigenAnalysisFilterType::Pointer m_EigenAnalysisFilter = EigenAnalysisFilterType::New();
+    m_EigenAnalysisFilter->SetDimension(IMAGE_DIMENSION);
+
+    // sheetness
+    FemurSheetnessFilterType::Pointer m_SheetnessFilter = FemurSheetnessFilterType::New();
+    m_SheetnessFilter->SetSheetnessNormalization(0.5); // = alpha
+    m_SheetnessFilter->SetBloobinessNormalization(0.5); // = beta
+    m_SheetnessFilter->SetNoiseNormalization(0.25); // = gamma
+    m_SheetnessFilter->SetDetectBrightSheets(true);
 
     // abs
     AbsFilterType::Pointer m_AbsFilter = AbsFilterType::New();
