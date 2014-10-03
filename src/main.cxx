88812ef034a4b1b4c1d25b86dfbab9b8ef2ce44b
Thomas Fitze <fitz@zhaw.ch>
2014-10-03 11:26:20 +0200

    removed Descoteaux code

--- src/main.cxx
+++ src/main.cxx
@@ -1,18 +1,4 @@
 // ITK
-#include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
-#include "itkImageSpatialObject.h"
-#include "itkHessianRecursiveGaussianImageFilter.h"
-#include "itkSymmetricEigenAnalysisImageFilter.h"
-#include "itkRescaleIntensityImageFilter.h"
-#include "itkCurvatureAnisotropicDiffusionImageFilter.h"
-#include "itkAbsImageFilter.h"
-#include "itkBinaryFunctorImageFilter.h"
-#include "itkMaximumImageFilter.h"
-
-#include "DescoteauxSheetnessImageFilter.h"
-#include "KrcahSheetnessImageFilter.h"
-#include "TraceFunctor.h"
 
 // pixel / image type
 const unsigned int IMAGE_DIMENSION = 3;
@@ -35,8 +21,7 @@ typedef itk::UnaryFunctorImageFilter<HessianImageType, InternalImageType, TraceF
 typedef itk::FixedArray<double, HessianPixelType::Dimension> EigenValueArrayType;
 typedef itk::Image<EigenValueArrayType, IMAGE_DIMENSION> EigenValueImageType;
 typedef itk::SymmetricEigenAnalysisImageFilter<HessianImageType, EigenValueImageType> EigenAnalysisFilterType;
-typedef itk::DescoteauxSheetnessImageFilter<EigenValueImageType, OutputImageType> SheetnessFilterType;
-typedef itk::KrcahSheetnessImageFilter<EigenValueImageType, OutputImageType> FemurSheetnessFilterType;
+typedef itk::KrcahSheetnessImageFilter<EigenValueImageType, OutputImageType> SheetnessFilter;
 typedef itk::RescaleIntensityImageFilter<OutputImageType, OutputImageType> RescaleFilterType;
 typedef itk::AbsImageFilter<InternalImageType, InternalImageType> AbsFilterType;
 typedef itk::Functor::Maximum<OutputImageType::PixelType, OutputImageType::PixelType, OutputImageType::PixelType> MaximumFunctorType;
@@ -46,8 +31,6 @@ typedef itk::BinaryFunctorImageFilter<OutputImageType, OutputImageType, OutputIm
 int process(char *in, char *out);
 
 FileReaderType::Pointer readImage(char *pathInput);
-
-OutputImageType::Pointer calculateDescoteauxSheetness(InputImageType::Pointer input, float sigma);
 
 OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, float sigma);
 
@@ -62,15 +45,6 @@ int process(char *inputPath, char *outputPath) {
     // read input
     FileReaderType::Pointer inputReader = readImage(inputPath);
     InputImageType::Pointer input = inputReader->GetOutput();
-//
-//    // get the sheetness for both sigma with descoteauxs sheetness implementation
-//    InternalImageType::Pointer resultSigma075Descoteaux = calculateDescoteauxSheetness(input, 0.75);
-//    InternalImageType::Pointer resultSigma100Descoteaux = calculateDescoteauxSheetness(input, 1.0);
-//
-//    // result(x,y) = max(resultSigma075Descoteaux(x,y), resultSigma100Descoteaux(x,y))
-//    MaximumFilterType::Pointer m_MaximumFilterDescoteaux = MaximumFilterType::New();
-//    m_MaximumFilterDescoteaux->SetInput1(resultSigma075Descoteaux);
-//    m_MaximumFilterDescoteaux->SetInput2(resultSigma100Descoteaux);
 
     // get the sheetness for both sigma with femur optimized sheetness implementation
     std::cout << "Processing with sigma=0.75..." << std::endl;
@@ -99,48 +73,6 @@ int process(char *inputPath, char *outputPath) {
     return EXIT_SUCCESS;
 }
 
-OutputImageType::Pointer calculateDescoteauxSheetness(InputImageType::Pointer input, float sigma) {
-    // anisotropic diffusion
-    DiffusionFilterType::Pointer m_DiffusionFilter = DiffusionFilterType::New();
-    m_DiffusionFilter->SetNumberOfIterations(10);
-    m_DiffusionFilter->SetTimeStep(0.06);
-    m_DiffusionFilter->SetConductanceParameter(2.0);
-
-    // hessian
-    HessianFilterType::Pointer m_HessianFilter = HessianFilterType::New();
-    m_HessianFilter->SetSigma(sigma);
-
-    // eigen analysis
-    EigenAnalysisFilterType::Pointer m_EigenAnalysisFilter = EigenAnalysisFilterType::New();
-    m_EigenAnalysisFilter->SetDimension(IMAGE_DIMENSION);
-
-    // sheetness
-    SheetnessFilterType::Pointer m_SheetnessFilter = SheetnessFilterType::New();
-    m_SheetnessFilter->SetSheetnessNormalization(0.5); // = alpha
-    m_SheetnessFilter->SetBloobinessNormalization(0.5); // = beta
-    m_SheetnessFilter->SetNoiseNormalization(1.0); // = gamma
-    m_SheetnessFilter->SetDetectBrightSheets(true);
-
-    // abs
-    AbsFilterType::Pointer m_AbsFilter = AbsFilterType::New();
-
-    // rescale
-    RescaleFilterType::Pointer m_RescaleFilter = RescaleFilterType::New();
-    m_RescaleFilter->SetOutputMinimum(0.0);
-    m_RescaleFilter->SetOutputMaximum(1.0);
-
-    // connect pipeline
-    m_DiffusionFilter->SetInput(input);
-    m_HessianFilter->SetInput(m_DiffusionFilter->GetOutput());
-    m_EigenAnalysisFilter->SetInput(m_HessianFilter->GetOutput());
-    m_SheetnessFilter->SetInput(m_EigenAnalysisFilter->GetOutput());
-    m_AbsFilter->SetInput(m_SheetnessFilter->GetOutput());
-    m_RescaleFilter->SetInput(m_AbsFilter->GetOutput());
-
-    m_RescaleFilter->Update();
-    return m_RescaleFilter->GetOutput();
-}
-
 OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, float sigma) {
     // anisotropic diffusion
     DiffusionFilterType::Pointer m_DiffusionFilter = DiffusionFilterType::New();
@@ -157,7 +89,7 @@ OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, 
     m_EigenAnalysisFilter->SetDimension(IMAGE_DIMENSION);
 
     // sheetness
-    FemurSheetnessFilterType::Pointer m_SheetnessFilter = FemurSheetnessFilterType::New();
+    SheetnessFilter::Pointer m_SheetnessFilter = SheetnessFilter::New();
     m_SheetnessFilter->SetSheetnessNormalization(0.5); // = alpha
     m_SheetnessFilter->SetBloobinessNormalization(0.5); // = beta
     m_SheetnessFilter->SetNoiseNormalization(0.25); // = gamma
