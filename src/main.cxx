d3f116455fe43f5fa490f89a21619c91ecc23c0a
Thomas Fitze <fitz@zhaw.ch>
2014-10-06 15:20:45 +0200

    added time measurements

--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,5 @@
+#include <ctime>
+
 // ITK
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
@@ -26,8 +28,10 @@ typedef itk::Image<OutputPixelType, IMAGE_DIMENSION> OutputImageType;
 typedef itk::ImageFileReader<InputImageType> FileReaderType;
 typedef itk::ImageFileWriter<OutputImageType> FileWriterType;
 
-// pre processing
+// input processing
 typedef itk::CurvatureAnisotropicDiffusionImageFilter<InputImageType, InternalImageType> DiffusionFilterType;
+
+// sheetness prerequisites
 typedef itk::HessianRecursiveGaussianImageFilter<InternalImageType> HessianFilterType;
 typedef HessianFilterType::OutputImageType HessianImageType;
 typedef HessianImageType::PixelType HessianPixelType;
@@ -94,52 +98,99 @@ int process(char *inputPath, char *outputPath) {
 }
 
 OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, float sigma) {
+    std::cout << "CPU time (in s):" << std::endl;
+
+    std::clock_t begin = std::clock();
+
     // anisotropic diffusion
     DiffusionFilterType::Pointer m_DiffusionFilter = DiffusionFilterType::New();
     m_DiffusionFilter->SetNumberOfIterations(10);
     m_DiffusionFilter->SetTimeStep(0.06);
     m_DiffusionFilter->SetConductanceParameter(2.0);
+    m_DiffusionFilter->SetInput(input);
+    m_DiffusionFilter->Update();
+
+    std::clock_t end = std::clock();
+    double elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Anisotropic diffusion: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // hessian
     HessianFilterType::Pointer m_HessianFilter = HessianFilterType::New();
     m_HessianFilter->SetSigma(sigma);
+    m_HessianFilter->SetInput(m_DiffusionFilter->GetOutput());
+    m_HessianFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Hessian: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // eigen analysis
     EigenAnalysisFilterType::Pointer m_EigenAnalysisFilter = EigenAnalysisFilterType::New();
     m_EigenAnalysisFilter->SetDimension(IMAGE_DIMENSION);
+    m_EigenAnalysisFilter->SetInput(m_HessianFilter->GetOutput());
+    m_EigenAnalysisFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Eigen analysis: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // calculate average trace
     TraceFilterType::Pointer m_TraceFilter = TraceFilterType::New();
     m_TraceFilter->SetImageDimension(IMAGE_DIMENSION);
+    m_TraceFilter->SetInput(m_HessianFilter->GetOutput());
+    m_TraceFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Trace: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     MeanProjectionFilterType::Pointer m_MeanProjectionFilter = MeanProjectionFilterType::New();
     m_MeanProjectionFilter->SetProjectionDimension(2);
+    m_MeanProjectionFilter->SetInput(m_TraceFilter->GetOutput());
+    m_MeanProjectionFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Mean projection: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // Sheetness
     SheetnessBroadcastingFilterType::Pointer m_SheetnessFilter = SheetnessBroadcastingFilterType::New();
+    m_SheetnessFilter->SetInput1(m_EigenAnalysisFilter->GetOutput());
+    m_SheetnessFilter->SetInput2(m_MeanProjectionFilter->GetOutput());
+    m_SheetnessFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Sheetness: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // abs
     AbsFilterType::Pointer m_AbsFilter = AbsFilterType::New();
+    m_AbsFilter->SetInput(m_SheetnessFilter->GetOutput());
+    m_AbsFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Absolute: " << elapsedSecs << std::endl;
+    begin = std::clock();
 
     // rescale
     RescaleFilterType::Pointer m_RescaleFilter = RescaleFilterType::New();
     m_RescaleFilter->SetOutputMinimum(0.0);
     m_RescaleFilter->SetOutputMaximum(1.0);
-
-    // connect pipeline
-    m_DiffusionFilter->SetInput(input);
-    m_HessianFilter->SetInput(m_DiffusionFilter->GetOutput());
-    m_EigenAnalysisFilter->SetInput(m_HessianFilter->GetOutput());
-    m_TraceFilter->SetInput(m_HessianFilter->GetOutput());
-    m_MeanProjectionFilter->SetInput(m_TraceFilter->GetOutput());
-
-    m_SheetnessFilter->SetInput1(m_EigenAnalysisFilter->GetOutput());
-    m_SheetnessFilter->SetInput2(m_MeanProjectionFilter->GetOutput());
-
-    m_AbsFilter->SetInput(m_SheetnessFilter->GetOutput());
     m_RescaleFilter->SetInput(m_AbsFilter->GetOutput());
-
     m_RescaleFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Rescale: " << elapsedSecs << std::endl;
+    std::cout << std::endl;
+
     return m_RescaleFilter->GetOutput();
 }
 
