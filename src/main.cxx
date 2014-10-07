0fd77ffd4c28f35dd85e07cb8a5146e97af210a8
Thomas Fitze <fitz@zhaw.ch>
2014-10-07 14:42:07 +0200

    test component based extraction

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,6 @@
 #include <ctime>
 
+
 // ITK
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
@@ -16,6 +17,12 @@
 #include "itkMaximumImageFilter.h"
 #include "itkMeanProjectionImageFilter.h"
 #include "itkRescaleIntensityImageFilter.h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkConnectedComponentImageFilter.h"
+#include <itkLabelToRGBImageFilter.h>
+#include <itkScalarConnectedComponentImageFilter.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkLabelMapToBinaryImageFilter.h>
 
 #include "KrcahSheetnessImageFilter.h"
 #include "TraceImageFilter.h"
@@ -57,14 +64,26 @@ typedef itk::Functor::Maximum<OutputPixelType, OutputPixelType, OutputPixelType>
 typedef itk::BinaryFunctorImageFilter<OutputImageType, OutputImageType, OutputImageType, MaximumFunctorType> MaximumFilterType;
 typedef itk::RescaleIntensityImageFilter<OutputImageType, OutputImageType> RescaleFilterType;
 
-// label map
+// femur connected components
+typedef itk::RGBPixel<unsigned char> RGBPixelType;
+typedef itk::Image<RGBPixelType, IMAGE_DIMENSION> RGBImageType;
+typedef itk::Image<unsigned long, IMAGE_DIMENSION> LabelImageType;
+typedef itk::ImageFileWriter<RGBImageType> RGBFileWriterType;
+typedef itk::Image<unsigned char, IMAGE_DIMENSION> BinaryImageType;
+typedef itk::ScalarConnectedComponentImageFilter<OutputImageType, LabelImageType> ConnectedComponentImageFilterType;
+typedef itk::RelabelComponentImageFilter<LabelImageType, LabelImageType> RelabelFilterType;
+typedef itk::BinaryThresholdImageFilter<LabelImageType, LabelImageType> BinaryThresholdFilterType;
+typedef itk::RescaleIntensityImageFilter<LabelImageType, OutputImageType> BinaryRescaleFilterType;
+
 
 // functions
-int process(char *in, char *out);
+int process(char *in, char *outputPathSheetness);
 
 FileReaderType::Pointer readImage(char *pathInput);
 
 OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, float sigma);
+
+OutputImageType::Pointer extractFemur(OutputImageType::Pointer input);
 
 // expected CLI call:
 // ./Testbench /path/to/input /path/to/output
@@ -73,7 +92,7 @@ int main(int argc, char *argv[]) {
     return ret;
 }
 
-int process(char *inputPath, char *outputPath) {
+int process(char *inputPath, char *outputPathSheetness) {
     // read input
     FileReaderType::Pointer inputReader = readImage(inputPath);
     InputImageType::Pointer input = inputReader->GetOutput();
@@ -102,23 +121,24 @@ int process(char *inputPath, char *outputPath) {
     m_RescaleFilter->SetOutputMaximum(1.0);
     m_RescaleFilter->SetInput(m_MaximumFilter->GetOutput());
 
-    // write result
-    std::cout << "writing to file..." << std::endl;
+    // write sheetness result
+    std::cout << "writing sheetness to file..." << std::endl;
     FileWriterType::Pointer writer = FileWriterType::New();
-    writer->SetFileName(outputPath);
-    writer->SetInput(m_RescaleFilter->GetOutput());
+    writer->SetFileName(outputPathSheetness);
+    writer->SetInput(resultSigma100Krcah);
     writer->Update();
 
     return EXIT_SUCCESS;
 }
 
 OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, float sigma) {
-    std::cout << "CPU time (in s):" << std::endl;
 
     /******
     * Input preprocessing
     ******/
+    std::cout << "CPU time (in s):" << std::endl;
     std::clock_t begin = std::clock();
+    std::clock_t beginTotal = std::clock();
 
     // I*G (anisotropic diffusion)
 //    AnisotropicDiffusionFilterType::Pointer m_DiffusionFilter = AnisotropicDiffusionFilterType::New();
@@ -230,9 +250,74 @@ OutputImageType::Pointer calculateKrcahSheetness(InputImageType::Pointer input, 
     end = std::clock();
     elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "Sheetness: " << elapsedSecs << std::endl;
+
+    std::clock_t endTotal = std::clock();
+    double elapsedSecsTotal = double(endTotal - beginTotal) / CLOCKS_PER_SEC;
+    std::cout << "------------" << std::endl;
+    std::cout << "Total: " << elapsedSecsTotal << std::endl;
     std::cout << std::endl;
 
     return m_SheetnessFilter->GetOutput();
+}
+
+OutputImageType::Pointer extractFemur(OutputImageType::Pointer input) {
+
+    std::cout << "CPU time (in s):" << std::endl;
+    std::clock_t begin = std::clock();
+    std::clock_t beginTotal = std::clock();
+
+    // connected components
+    ConnectedComponentImageFilterType::Pointer m_ConnectedCompontentFilter = ConnectedComponentImageFilterType::New();
+    m_ConnectedCompontentFilter->SetDistanceThreshold(0.10);
+    m_ConnectedCompontentFilter->SetInput(input);
+    m_ConnectedCompontentFilter->Update();
+
+    std::clock_t end = std::clock();
+    double elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "connected components: " << elapsedSecs << std::endl;
+    begin = std::clock();
+
+    // filter by size
+    RelabelFilterType::Pointer m_RelabelFilter = RelabelFilterType::New();
+    m_RelabelFilter->SetMinimumObjectSize(1500);
+    m_RelabelFilter->SetInput(m_ConnectedCompontentFilter->GetOutput());
+    m_RelabelFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "relabel: " << elapsedSecs << std::endl;
+    begin = std::clock();
+
+    // threshold
+    BinaryThresholdFilterType::Pointer m_ThresholdFilter = BinaryThresholdFilterType::New();
+    m_ThresholdFilter->SetLowerThreshold(4);
+    m_ThresholdFilter->SetInput(m_RelabelFilter->GetOutput());
+    m_ThresholdFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "treshold: " << elapsedSecs << std::endl;
+    begin = std::clock();
+
+    // rescale to range 0.0 - 1.0
+    BinaryRescaleFilterType::Pointer m_RescaleFilter = BinaryRescaleFilterType::New();
+    m_RescaleFilter->SetOutputMinimum(0.0);
+    m_RescaleFilter->SetOutputMaximum(1.0);
+    m_RescaleFilter->SetInput(m_ThresholdFilter->GetOutput());
+    m_RescaleFilter->Update();
+
+    end = std::clock();
+    elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "rescale: " << elapsedSecs << std::endl;
+    begin = std::clock();
+
+    std::clock_t endTotal = std::clock();
+    double elapsedSecsTotal = double(endTotal - beginTotal) / CLOCKS_PER_SEC;
+    std::cout << "------------" << std::endl;
+    std::cout << "Total: " << elapsedSecsTotal << std::endl;
+    std::cout << std::endl;
+
+    return m_RescaleFilter->GetOutput();
 }
 
 FileReaderType::Pointer readImage(char *path) {
