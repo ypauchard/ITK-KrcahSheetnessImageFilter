deba08c10c7772c84fcaf39da7abeffc2e5fc32c
Thomas Fitze <fitz@zhaw.ch>
2014-10-09 14:15:56 +0200

    removed unneeded Update calls

--- src/KrcahSheetnessFeatureGenerator.hxx
+++ src/KrcahSheetnessFeatureGenerator.hxx
@@ -29,9 +29,9 @@ namespace itk {
         typename MaximumAbsoluteValueFilterType::Pointer maximumAbsoluteValueFilter = MaximumAbsoluteValueFilterType::New();
         maximumAbsoluteValueFilter->SetInput1(resultSigma075);
         maximumAbsoluteValueFilter->SetInput2(resultSigma100);
-        maximumAbsoluteValueFilter->Update();
 
         // copy output from last filter
+        maximumAbsoluteValueFilter->Update();
         this->GetOutput()->Graft(maximumAbsoluteValueFilter->GetOutput());
     }
 
@@ -43,31 +43,26 @@ namespace itk {
         ******/
         typename InputCastFilterType::Pointer castFilter = InputCastFilterType::New();
         castFilter->SetInput(input);
-        castFilter->Update();
 
         // I*G (discrete gauss)
         typename GaussianFilterType::Pointer m_DiffusionFilter = GaussianFilterType::New();
         m_DiffusionFilter->SetVariance(1);
         m_DiffusionFilter->SetInput(castFilter->GetOutput());
-        m_DiffusionFilter->Update();
 
         // I - (I*G)
         typename SubstractFilterType::Pointer m_SubstractFilter = SubstractFilterType::New();
         m_SubstractFilter->SetInput1(castFilter->GetOutput());
         m_SubstractFilter->SetInput2(m_DiffusionFilter->GetOutput()); // =s
-        m_SubstractFilter->Update();
 
         // k(I-(I*G))
         typename MultiplyFilterType::Pointer m_MultiplyFilter = MultiplyFilterType::New();
         m_MultiplyFilter->SetInput(m_SubstractFilter->GetOutput());
         m_MultiplyFilter->SetConstant(10); // =k
-        m_MultiplyFilter->Update();
 
         // I+k*(I-(I*G))
         typename AddFilterType::Pointer m_AddFilter = AddFilterType::New();
         m_AddFilter->SetInput1(castFilter->GetOutput());
         m_AddFilter->SetInput2(m_MultiplyFilter->GetOutput());
-        m_AddFilter->Update();
 
         /******
         * sheetness prerequisites
@@ -76,24 +71,21 @@ namespace itk {
         typename HessianFilterType::Pointer m_HessianFilter = HessianFilterType::New();
         m_HessianFilter->SetSigma(sigma);
         m_HessianFilter->SetInput(m_AddFilter->GetOutput());
-        m_HessianFilter->Update();
 
         // eigen analysis
         typename EigenAnalysisFilterType::Pointer m_EigenAnalysisFilter = EigenAnalysisFilterType::New();
         m_EigenAnalysisFilter->SetDimension(NDimension);
         m_EigenAnalysisFilter->SetInput(m_HessianFilter->GetOutput());
-        m_EigenAnalysisFilter->Update();
 
         // calculate trace
         typename TraceFilterType::Pointer m_TraceFilter = TraceFilterType::New();
         m_TraceFilter->SetImageDimension(NDimension);
         m_TraceFilter->SetInput(m_HessianFilter->GetOutput());
-        m_TraceFilter->Update();
 
         // calculate average
         typename StatisticsFilterType::Pointer m_StatisticsFilter = StatisticsFilterType::New();
         m_StatisticsFilter->SetInput(m_TraceFilter->GetOutput());
-        m_StatisticsFilter->Update();
+        m_StatisticsFilter->Update(); // needed! ->GetMean() will not trigger an update!
 
         /******
         * Sheetness
@@ -101,6 +93,8 @@ namespace itk {
         typename SheetnessFilterType::Pointer m_SheetnessFilter = SheetnessFilterType::New();
         m_SheetnessFilter->SetInput(m_EigenAnalysisFilter->GetOutput());
         m_SheetnessFilter->SetConstant(m_StatisticsFilter->GetMean());
+
+        // return
         m_SheetnessFilter->Update();
         return m_SheetnessFilter->GetOutput();
     }
